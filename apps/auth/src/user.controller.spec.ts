c31c290a2fd0fae0cbe76078a728ef81613859b5
Bhaskar4u <bhaskarsahni121@gmail.com>
2026-01-30 23:20:53 +0530

    did bcryptjs at mock(bcrypt)

--- apps/auth/src/user.controller.spec.ts
+++ apps/auth/src/user.controller.spec.ts
@@ -7,12 +7,11 @@ import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import * as bcrypt from 'bcryptjs';
 
-
 const mockUserRepository = () => ({
   findOne: jest.fn(),
   save: jest.fn(),
 });
-jest.mock('bcrypt', () => ({
+jest.mock('bcryptjs', () => ({
   hash: jest.fn().mockResolvedValue('hashed_password'),
   compare: jest.fn().mockResolvedValue(true),
 }));
